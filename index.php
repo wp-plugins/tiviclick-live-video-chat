a620e0c660561814d044f6b26daf9ed5ac7d34ca
Iftach Ben Zvi <Iftach Ben Zvi@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-11-19 16:08:57 +0000

    20131119 1.0.1
    git-svn-id: https://plugins.svn.wordpress.org/tiviclick-live-video-chat/tags/1.0.1@806888 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * @package TiviClick Settings
+ */
+/*
+Plugin Name: TiviClick
+Plugin URI: http://www.tiviclick.com/addons/wordpress/tiviclick.zip
+Description: This plugin will add Tiviclick video chat to your wordpress website. A Tiviclick account is required.
+Version: 1.0.1
+Author: TiviClick <admin@tiviclick.com>
+Author URI: http://www.tiviclick.com
+*/
+
+// ############ CREATING SETTING PAGES ON THE INSTALL OF THIS PLUGIN.. ############# //
+
+define('TIVICLICK_URL', plugins_url());
+define('TIVICLICK_DIR', plugin_dir_path( __FILE__ ));
+
+include_once( TIVICLICK_DIR . 'functions.php' );
